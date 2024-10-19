7abc7fa82214d38574b6fe258365b10312e82c7f
LeanidRymkevich <leanidrymkevich@gmail.com>
2024-10-19 22:58:49 +0300

    feat: setup db connection

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 
+import db from './db/db';
+
 import { ENDPOINTS } from './types/enums';
 
 import { LOCALES_ROOT_FOLDER } from './constants/constants';
@@ -8,10 +10,12 @@ import { LOCALES_ROOT_FOLDER } from './constants/constants';
 const app = async (port: string): Promise<void> => {
   const server = express();
 
+  server.use(express.json());
   server.use(cors());
 
   server.use(ENDPOINTS.LOCALES, express.static(LOCALES_ROOT_FOLDER));
 
+  await db.sync();
   server.listen(port, (): void => {
     console.log(`Server running on the port ${port}`);
   });
---
+++ src/db/db.ts
@@ -0,0 +1,15 @@
+import { Sequelize } from 'sequelize-typescript';
+import { Options } from 'sequelize';
+
+import configs from './config/config';
+
+import { DevelopmentMode } from '../types/types';
+
+const mode: DevelopmentMode =
+  (process.env.NODE_ENV as DevelopmentMode) || 'development';
+const config: Options = configs[mode];
+const db = new Sequelize(config);
+
+// db.addModels([Admin]);
+
+export default db;

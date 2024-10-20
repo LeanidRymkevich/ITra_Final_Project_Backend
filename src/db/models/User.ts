f12f8b17d708fe3efb24c4203b6d8382e7dcf465
LeanidRymkevich <leanidrymkevich@gmail.com>
2024-10-20 16:14:16 +0300

    fix: add index on email and ENUM type to user model

--- src/db/models/User.ts
+++ src/db/models/User.ts
@@ -3,48 +3,52 @@ import { DataTypes } from 'sequelize';
 import { USER_ROLES, USER_STATUS } from '../../types/enums';
 import db from '../db';
 
-const User = db.define('User', {
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      notEmpty: true,
+const User = db.define(
+  'User',
+  {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        notEmpty: true,
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    role: {
+      type: DataTypes.ENUM(USER_ROLES.ADMIN, USER_ROLES.USER),
+      allowNull: false,
+      defaultValue: USER_ROLES.USER,
+    },
+    status: {
+      type: DataTypes.ENUM(USER_STATUS.ACTIVE, USER_STATUS.BLOCKED),
+      allowNull: false,
+      defaultValue: USER_STATUS.ACTIVE,
     },
   },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-    validate: {
-      notEmpty: true,
-      isEmail: true,
-    },
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      notEmpty: true,
-    },
-  },
-  role: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    defaultValue: USER_ROLES.USER,
-    validate: {
-      notEmpty: true,
-      isIn: [[USER_ROLES.ADMIN, USER_ROLES.USER]],
-    },
-  },
-  status: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    defaultValue: USER_STATUS.ACTIVE,
-    validate: {
-      notEmpty: true,
-      isIn: [[USER_STATUS.ACTIVE, USER_STATUS.BLOCKED]],
-    },
-  },
-});
+  {
+    indexes: [
+      {
+        unique: true,
+        fields: ['email'],
+      },
+    ],
+    timestamps: false,
+  }
+);
 
 export default User;

5ce84660b237d55af707402cdb347633cc49b660
LeanidRymkevich <leanidrymkevich@gmail.com>
2024-10-23 15:25:22 +0300

    feat: add possibility of pagination on Users

--- src/controllers/adminController.ts
+++ src/controllers/adminController.ts
@@ -10,13 +10,15 @@ import { ID_ENDPOINTS_PARAM } from '../constants/constants';
 import CustomError from '../errors/CustomError';
 import { ERROR_MSGs } from '../types/enums';
 
-const getAllUsers = async (_req: Request, resp: Response): Promise<void> => {
-  const users: User[] = await User.findAll();
-  respWithData(
-    resp,
-    StatusCodes.OK,
-    users.map((user) => delPasswordField(user))
-  );
+const getAllUsers = async (req: Request, resp: Response): Promise<void> => {
+  const { limit, offset } = req.body;
+
+  const total = await User.count();
+  const users: User[] = await User.findAll({ limit, offset });
+  respWithData(resp, StatusCodes.OK, {
+    users: users.map((user) => delPasswordField(user)),
+    total,
+  });
 };
 
 const updateUser = async (req: Request, resp: Response): Promise<void> => {
